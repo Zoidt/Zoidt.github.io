20822fc7a3433c29d302183b3591c9f5faf292a8
ZaidT <zaidtabana@gmail.com>
2025-08-13 10:41:14 -0400

    move files out of project folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-@@ .. @@
- import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
- export default defineConfig({
--  site: 'https://yourusername.github.io', // Replace with your GitHub username
-+  site: 'https://zoidt.github.io',
-   integrations: [],
-   markdown: {
-     shikiConfig: {
-       theme: 'github-dark'
-     }
-   }
- });
+export default defineConfig({
+  site: 'https://yourusername.github.io', // Replace with your GitHub username
+  integrations: [],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
+});
