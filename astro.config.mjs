a4cce4d68222421dd5c938c296cc284b9a74ee08
Zaid Tabana <zaidtabana@gmail.com>
2025-08-13 10:38:23 -0400

    Configure Astro site for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+@@ .. @@
+ import { defineConfig } from 'astro/config';
+
+ export default defineConfig({
+-  site: 'https://yourusername.github.io', // Replace with your GitHub username
++  site: 'https://zoidt.github.io',
+   integrations: [],
+   markdown: {
+     shikiConfig: {
+       theme: 'github-dark'
+     }
+   }
+ });
